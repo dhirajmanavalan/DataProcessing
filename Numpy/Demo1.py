d5099b3c717680388c997a73eaf778c0e68d144d
Dhirajkumar <kumardhiraj7720@gmail.com>
2026-01-20 14:34:34 +0530

    datatypes , array reshaping , type casting

--- Numpy/Demo1.py
+++ Numpy/Demo1.py
@@ -43,5 +43,73 @@ import numpy as np
 # a = np.random.randn(2,3)
 # print(a)
 
-a=np.random.randint(10,100, size=(2,4))
-print(a)
+# a=np.random.randint(10,100, size=(2,4))
+# print(a)
+
+# a = np.array([1, 2, 3, 4])
+# print(a.dtype)
+# print(type(a))
+
+# lst = ['string',1.2,True,1,2]
+# arr = np.array(lst)
+# print(arr)
+# print(type(arr))
+# print(arr.dtype)
+
+# a = np.array([1, 2, 3, 4])
+# print(a.dtype)
+# print(a)
+
+
+# a = np.array([1, 2, 3, 4, 5], dtype=np.float32)
+# print(a.dtype)
+# print(a)
+
+# arr = np.array([1,2,3])
+# new_arr = arr.astype(np.float64)
+# print(arr.dtype)
+# print(arr)
+# print(new_arr.dtype)
+# print(new_arr)
+
+# arr = np.array([1.2,2.6,3.9])
+# new_arr = arr.astype(np.int64)
+# print(arr.dtype)
+# print(arr)
+# print(new_arr.dtype)
+# print(new_arr)
+
+# arr = np.array(["1","3.2","Dhiraj"])
+# arr_2 = arr.astype(np.int64)
+# print(arr_2)
+
+# ar = np.array([1, 2, 3, 4, 5, 6])
+# res = ar.reshape(3, 2)
+# # print(res)
+
+# res2 = res.reshape(2, 3)
+# print(res2)
+
+# res3 = res2.ravel()
+# print(res3)
+
+# res3[0] = 10
+# print(res3)
+
+# ar = np.array([1, 2, 3, 4, 5, 6])
+# ar2 = ar.reshape(2, 3)
+# print(ar2)
+
+# ar3 = ar2.ravel()
+# print(ar3)
+
+# ar3[0] = 10
+# print(ar3)
+# print(ar)
+
+# ar3 = ar2.flatten()
+# print(ar3)
+
+# ar3[0] = 10
+# print(ar3)
+# print(ar)
