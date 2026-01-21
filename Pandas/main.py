cdeb5fc2f8164877beac0e3f37c9b178705ec74a
Dhirajkumar <kumardhiraj7720@gmail.com>
2026-01-21 21:15:37 +0530

    series completed

--- Pandas/main.py
+++ Pandas/main.py
@@ -45,3 +45,41 @@ import pandas as pd
 # # print(s.loc["a":"d"])
 
 # print(s.loc["a"])
+
+
+fruit_protein = {
+    "lemon" : 20,
+    "apple" : 2,
+    "orange" :4
+}
+
+print(pd.Series(fruit_protein, name="protein"))
+
+
+fruits = {"mango": 1.0, "orange": 2.3, "apple": 0.49, "pineapple": 3.9, "avacoda": 3}
+
+s = pd.Series(fruits, name="proteins")
+# print(s)
+
+print(s > 1)
+
+print(s[s>1])
+
+
+fruits = {
+    "apple": 0.2,
+    "banana": 1.2,
+    "orange": 2.4,
+    "mango": 3.4,
+    "pineapple": 1.9,
+}
+
+s = pd.Series(fruits, name="proteins")
+print(s)
+
+print(s[(s > 1) & (s < 3)])
+
+s['orange'] = 2.33
+
+print(s)
+
