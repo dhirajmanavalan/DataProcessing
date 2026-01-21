66c610b2d4f295521d68baf07b722e92ddee7259
Dhirajkumar <kumardhiraj7720@gmail.com>
2026-01-21 16:54:33 +0530

    practiced pandas basic concepts

---
+++ Pandas/main.py
@@ -0,0 +1,47 @@
+import pandas as pd
+
+# s = pd.Series([1, 2, 3, 4, 5])
+# # print(s)
+# # print(s.index)
+# # print(s.values)
+# s.name = "numbers"
+# # print(s.name)
+# print(s)
+
+
+# s=pd.Series([12,32,14,441])
+# s.name = "Numbers_dhiru"
+# print(s)
+# print(s.index)
+# print(s.values)
+# print(s.name)
+
+# s = pd.Series([1,2,3,4,5])
+# print(s[1:4])
+
+# s = pd.Series([10, 20, 30, 40, 50])
+# print(s.iloc[[1, 2, 3]])
+
+# s = pd.Series([100, 120, 200, 270, 300])
+# index = ["apple", "banana", "grape", "orange", "mango"]
+# s.index = index
+# s.name = "calories"
+# # print(s)
+
+# # print(index[1:3])
+# # print(s.iloc[[2,4]])
+# print(s.loc["grape"])
+
+# s = pd.Series([100, 120, 130, 140, 150])
+
+# index = ["a", "b", "c", "d", "e"]
+
+# s.index = index
+# s.name = "calories"
+
+# # print(s)
+
+# # print(s.iloc[1:4])
+# # print(s.loc["a":"d"])
+
+# print(s.loc["a"])
